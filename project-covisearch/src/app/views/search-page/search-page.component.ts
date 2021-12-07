ae973b525f5227a329bbf214edf71200cc699de8
Aniket Bharti <aniket.bharti0697@gmail.com>
2021-12-06 21:04:52 -0500

    ui changes

--- project-covisearch/src/app/views/search-page/search-page.component.ts
+++ project-covisearch/src/app/views/search-page/search-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpService } from 'src/app/services/http.service';
 import { environment } from 'src/environments/environment';
+import * as _ from 'lodash'
 
 @Component({
   selector: 'app-search-page',
@@ -25,15 +26,48 @@ export class SearchPageComponent implements OnInit {
     this.activedRoute.queryParams
       .subscribe(params => {
         this.query = params['query']
-        console.log(this.query)
         this.httpService.postMethod(environment.news, { query: this.query }).subscribe((res) => {
           this.newList = res['response']
+          this.newList.forEach((element) => {
+            delete element['sentiments']['compound']
+            let data = _(element['sentiments'])
+              .toPairs()
+              .orderBy([1], ['desc'])
+              .fromPairs()
+              .value()
+            let key = Object.keys(data)[0]
+            let displaySentiment = null
+            if (key == 'neu') {
+              displaySentiment = ['Neutral', data[key]]
+            } else if (key == 'neg') {
+              displaySentiment = ['Negative', data[key]]
+            } if (key == 'pos') {
+              displaySentiment = ['Postive', data[key]]
+            }
+            element['display'] = displaySentiment
+          })
           console.log(this.newList)
         })
         this.httpService.postMethod(environment.search, { query: this.query }).subscribe((res) => {
           this.searchResultNonPOI = []
           this.searchResultPOI = []
           res['response']['docs'].forEach((element: any) => {
+            delete element['sentiments']['compound']
+            let data = _(element['sentiments'])
+              .toPairs()
+              .orderBy([1], ['desc'])
+              .fromPairs()
+              .value()
+            let key = Object.keys(data)[0]
+            let displaySentiment = null
+            if (key == 'neu') {
+              displaySentiment = ['Neutral', data[key]]
+            } else if (key == 'neg') {
+              displaySentiment = ['Negative', data[key]]
+            } if (key == 'pos') {
+              displaySentiment = ['Postive', data[key]]
+            }
+            element['display'] = displaySentiment
             if ('poi_name' in element) {
               this.searchResultPOI.push(element)
             }
@@ -45,21 +79,6 @@ export class SearchPageComponent implements OnInit {
       }
       );
   }
-
-
-
-  // country: "USA"
-  // id: "1440753313801068554"
-  // mentions: ['WHOIraq']
-  // replied_to_user_id: 982219486248910800
-  // score: 3.657134
-  // sentiments: {polarity: 0, sentiment: 'Neutral', subjectivity: 0}
-  // text_en: "daily COVID-19 epidemic situation in Iraq 22 September 2021"
-  // tweet_date: "2021-09-22T19:00:00Z"
-  // tweet_lang: "en"
-  // tweet_text: "@WHOIraq daily COVID-19 epidemic situation in Iraq\n22 September 2021 https://t.co/ckYc6veFYN"
-  // verified: true
-  // _version_: 1718369006134493200
 
   changeDisplay(mode: Number): void {
     this.display = mode;
