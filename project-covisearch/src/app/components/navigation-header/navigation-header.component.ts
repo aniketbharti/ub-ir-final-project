258b4c998d87a47c9e2ea99e1e18db7ce2ca732a
ANIKET BHARTI <ANIKET.BHARTI@lntinfotech.com>
2021-12-13 00:33:47 +0530

    commit changes

--- project-covisearch/src/app/components/navigation-header/navigation-header.component.ts
+++ project-covisearch/src/app/components/navigation-header/navigation-header.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-header',
   templateUrl: './navigation-header.component.html',
-  styleUrls: ['./navigation-header.component.scss']
+  styleUrls: ['./navigation-header.component.scss'],
 })
 export class NavigationHeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.url.subscribe((url) => console.log(url[0].path));
   }
-
 }
