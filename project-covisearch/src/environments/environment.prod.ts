3c0a36891a016a7042d7a72b30431f05ea22dbbe
ANIKET BHARTI <ANIKET.BHARTI@lntinfotech.com>
2021-12-12 21:53:05 +0530

    envirnoment fix

--- project-covisearch/src/environments/environment.prod.ts
+++ project-covisearch/src/environments/environment.prod.ts
@@ -1,8 +1,18 @@
+const baseUrl = "http://3.144.209.214:5000/api/"
+const wikipediaUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
+
 const url = {
   countries: "https://corona.lmao.ninja/v2/countries",
-  total: "https://disease.sh/v3/covid-19/all"
+  total: "https://disease.sh/v3/covid-19/all",
+  search: baseUrl + 'search',
+  news: baseUrl + 'getnews',
+  countrytweet: baseUrl + 'country',
+  pois: baseUrl + 'pois',
+  wiki: wikipediaUrl,
+  global: baseUrl + 'global'
 }
+
 export const environment = {
-  production: false,
-  ...url
+  production: true,
+  ...url,
 };
